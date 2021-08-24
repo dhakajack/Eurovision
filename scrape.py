0efae9769f3792002b2c4ae2918fd3b6a5a7503f
dhakajack <dhakajack@gmail.com>
2021-08-24 09:07:05 +0300

    save as timestamped file

--- scrape.py
+++ scrape.py
@@ -1,18 +1,20 @@
 """
 Download every trial publicly listed on the EU Clinical trial register,
-page by page as a huge text file. When this was written, that was the
-only option available.
+page by page as a huge text file.
 """
 
 import requests
 import time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+# To not have to deal with the SSL connection, certs, etc.
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-TOP_PAGE = 5  #one more than last page
+TOP_PAGE = 5  # one more than last page
 URL = "https://www.clinicaltrialsregister.eu/ctr-search/rest/download/full?query=&page={}&mode=current_page"
 
-def sleep_on_error(error_type:str, sleep_duration) -> int:
+
+def sleep_on_error(error_type: str, sleep_duration: int) -> int:
     print("{} Error. Resetting after {} seconds".format(error_type, sleep_duration))
     time.sleep(sleep_duration)
     if sleep_duration < 300:
@@ -20,21 +22,22 @@ def sleep_on_error(error_type:str, sleep_duration) -> int:
     return sleep_duration
 
 
-filespec = input("Name of file to store downloaded results? >")
 start_time = time.time()
+filespec = time.strftime("%Y%m%d-%H%M")
 print("Executing")
 with open(filespec, "w") as test_file:
     for page_number in range(1, TOP_PAGE):
         print("Accessing page {}".format(page_number))
         print("### PAGE {} ####".format(page_number), file=test_file)
         read_again = True
+        # noinspection PyRedeclaration
         sleep_time = 1
         while read_again:
             try:
                 # timeout parameters: time to connect, time to begin reading response
-                page = requests.get(URL.format(page_number), verify=False, timeout=(2,5))
+                page = requests.get(URL.format(page_number), verify=False, timeout=(2, 5))
             except requests.exceptions.Timeout:
-                sleep_time = sleep_on_error("Time Out",sleep_time)
+                sleep_time = sleep_on_error("Time Out", sleep_time)
             except requests.exceptions.ConnectionError:
                 sleep_time = sleep_on_error("Connection", sleep_time)
             else:
