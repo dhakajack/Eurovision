5c15901c87c4b1219dd98bbba4a025bdede98b96
dhakajack <dhakajack@gmail.com>
2021-09-10 20:21:34 +0300

    revise to work with new scan version

--- toexcel.py
+++ toexcel.py
@@ -3,10 +3,23 @@ import sqlite3
 
 
 def flatten(t):
+    """
+    Returns a flat list.
+    :param t: An arbitrarily nested sequence.
+    :return: List.
+    """
     return [item for sublist in t for item in sublist]
 
 
-def search_a_table(table: str, the_cursor) -> bool:
+def search_a_table(table: str, the_cursor: sqlite3.Cursor) -> bool:
+    """
+    Asks user for search parameters for a given table. Puts eudract number
+    for any matching trial into the respective result set.
+    :param table: A database table.
+    :param the_cursor: A database cursor.
+    :return: Returns true unless no parameter is entered for a table other than "trial".
+    """
+
     search_term = input('{} Data: Enter a WHERE clause > '.format(table.title()))
     if search_term == "":
         if table == "trial":            # trial is guaranteed to have an entry for all trials
@@ -20,29 +33,7 @@ def search_a_table(table: str, the_cursor) -> bool:
     return True
 
 
-trial_terms = ("eudract", "status", "db_date", "title", "nct", "placebo", "condition",
-               "meddra_version", "meddra_level", "meddra_classification",
-               "meddra_term", "meddra_soc", "rare", "fih", "bioequivalence",
-               "phase1", "phase2", "phase3", "phase4", "diagnosis", "prophylaxis",
-               "therapy", "safety", "efficacy", "pk", "pd", "randomised",
-               "open_design", "single_blind", "double_blind", "crossover",
-               "age_in_utero", "age_preterm", "age_newborn", "age_under2",
-               "age_2to11", "age12to17", "age18to64", "age_65plus", "female",
-               "male", "network", "eot_date")
-
-drug_terms = ("trade", "product", "code", "inn", "cas", "sponsor_code", "ev_substance",
-              "alt_names")
-
-sponsor_terms = ("name", "org", "contact", "email")
-
-trial_term_string = ", ".join(trial_terms)
-drug_term_string = ", ".join(drug_terms)
-sponsor_term_string = "name"
-location_term_string = "location"
-
-database = "big6"
-
-drug_list = []
+database = "big7"
 
 result_set = {"trial": set(),
               "drug": set(),
@@ -51,7 +42,11 @@ result_set = {"trial": set(),
               }
 
 final_set = {}
-
+drug_list = []
+display_trial = ["eudract", "title", "condition"]  # TO CONSIDER: make this a customizable list
+trial_terms_string = ", ".join(display_trial)
+drug_terms = ("trade", "product", "code")
+drug_term_string = ", ".join(drug_terms)
 
 db = sqlite3.connect(database)
 cursor = db.cursor()
@@ -63,30 +58,35 @@ while True:
     drug_flag = search_a_table("drug", cursor)
     location_flag = search_a_table("location", cursor)
     sponsor_flag = search_a_table("sponsor", cursor)
+
+    # Intersect result sets from search on each table to yield a final set
     final_set = result_set["trial"]
-    if drug_flag:
-        final_set &= result_set["drug"]
+    if drug_flag:                            # only bother intersecting with drug table result if a search parameter
+        final_set &= result_set["drug"]      # was provided; i.e., do not narrow the search if not drug parameter.
     if location_flag:
-        final_set &= result_set["location"]
+        final_set &= result_set["location"]  # same for location and sponsor.
     if sponsor_flag:
         final_set &= result_set["sponsor"]
+
+    # Summarize result of search and ask user whether it is worthwhile to dump result to an excel spreadsheet
     print("Final data set includes {} trials".format(len(final_set)))
     output_file = input('Enter file name for output or "A" to abort >')
     if output_file.casefold() == "a":
         print("Aborted.")
         continue
+
+    # Create a new excel spreadsheet with the result set
     else:
         wb = Workbook()
         ws = wb.active
-        ws.title = "Test Record"
-        headers = list(trial_terms)
-        headers.append("drug")
-        headers.append("location")
-        headers.append("sponsor")
+        ws.title = "Test Record"  # Later, make this more interesting. Consider adding search terms to another tab.
+        headers = display_trial[::]
+        headers.extend(("drug", "location", "sponsor"))
         ws.append(headers)
+
         for trial_selected in sorted(final_set):
             cursor.execute("SELECT {} FROM trial WHERE eudract = \"{}\"".
-                           format(trial_term_string, trial_selected))
+                           format(trial_terms_string, trial_selected))
             extract = cursor.fetchone()
             trial_data = list(extract)
             cursor.execute("SELECT {} FROM drug WHERE eudract = \"{}\""
@@ -94,31 +94,21 @@ while True:
             rows = cursor.fetchall()
             drug_list.clear()
             for drug_data in rows:
-                if drug_data[3]:            # prefer INN
-                    drug_name_source = 3
-                elif drug_data[1]:          # product name
+                if drug_data[1]:            # prefer product name
                     drug_name_source = 1
-                elif drug_data[0]:          # trade name
+                elif drug_data[0]:          # product trade name
                     drug_name_source = 0
-                elif drug_data[2]:          # product code
-                    drug_name_source = 2
-                elif drug_data[4]:          # CAS number
-                    drug_name_source = 4
-                elif drug_data[6]:          # EV substance code
-                    drug_name_source = 6
-                elif drug_data[7]:          # alternative names
-                    drug_name_source = 7
                 else:
-                    drug_name_source = 5    # sponsor code
+                    drug_name_source = 2    # product code
                 drug_list.append(drug_terms[drug_name_source] + ":" + drug_data[drug_name_source])
             drug_entry = "; ".join(drug_list)
-            cursor.execute("SELECT {} FROM location WHERE eudract = \"{}\""
-                           .format(location_term_string, trial_selected))
+            cursor.execute("SELECT location FROM location WHERE eudract = \"{}\""
+                           .format(trial_selected))
             rows = cursor.fetchall()
             location_entry = ", ".join(flatten(rows))
-            cursor.execute("SELECT {} FROM sponsor WHERE eudract = \"{}\""
-                           .format(sponsor_term_string, trial_selected))
-            sponsor_entry = cursor.fetchone()[0]  # sponsor name
+            cursor.execute("SELECT name FROM sponsor WHERE eudract = \"{}\""
+                           .format(trial_selected))
+            sponsor_entry = cursor.fetchone()[0]
             trial_data.append(drug_entry)
             trial_data.append(location_entry)
             trial_data.append(sponsor_entry)
